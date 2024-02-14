a4e8494fd7aa5b233108be99ae4ad495e37e668f
Austin Riba <austin@m51.io>
2024-02-13 20:52:59 -0800

    Implementation for inline validation

--- patterns/urls.py
+++ patterns/urls.py
@@ -1,11 +1,26 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from patterns.views import simple_forms
+from patterns.views import inline_validation, simple_forms
 
 app_name = "patterns"
 urlpatterns = [
     path("", TemplateView.as_view(template_name="patterns/index.html"), name="index"),
     path("simple_forms/", simple_forms.index_view, name="simple-form-index"),
     path("simple_forms/form", simple_forms.form_view, name="simple-form"),
+    path(
+        "inline_validation/",
+        inline_validation.index_view,
+        name="inline-validation-index",
+    ),
+    path(
+        "inline_validation/form",
+        inline_validation.form_view,
+        name="inline-validation-form",
+    ),
+    path(
+        "inline_validation/username",
+        inline_validation.username_validation_view,
+        name="inline-validation-username",
+    ),
 ]
---
+++ patterns/views/inline_validation.py
@@ -0,0 +1,52 @@
+from django import forms
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def valid_username(username: str) -> bool:
+    """
+    Validation logic would go here most likely a database
+    lookup. This example just compares the string to
+    "fred".
+    """
+    return username.lower() == "fred"
+
+
+class RegistrationForm(forms.Form):
+    username = forms.CharField(max_length=200)
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        if not valid_username(username):
+            raise forms.ValidationError("That username is taken.")
+        return username
+
+
+def form_view(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            message = "Registration successful!"
+            form = RegistrationForm()  # reset the form
+        else:
+            message = "Please correct the above errors to continue."
+        return render(
+            request,
+            "patterns/inline_validation/form.html",
+            {"form": form, "message": message},
+        )
+
+
+def username_validation_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        if not valid_username(username):
+            return HttpResponse(b"<li>That username is taken.</li>")
+        else:
+            return HttpResponse(b"")
+
+
+def index_view(request):
+    form = RegistrationForm()
+    return render(request, "patterns/inline_validation/index.html", {"form": form})
